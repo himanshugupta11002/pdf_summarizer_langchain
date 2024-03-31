ac1defc39816bd09653804d5fbf43d6137ee94b1
himanshugupta11002 <72141497+himanshugupta11002@users.noreply.github.com>
2024-03-31 05:45:23 +0000

    update

--- pdf.py
+++ pdf.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
-from streamlit_extras.add_vertical_space import add_vertical_space
+#from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -22,7 +22,7 @@ with st.sidebar:
     - [OpenAI](https://platform.openai.com/docs/models) LLM model
 
     ''')
-    add_vertical_space(5)
+    #add_vertical_space(5)
 
 load_dotenv()
 
