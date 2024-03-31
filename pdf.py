ea79518ed576d6eef1e0935f742ccedac812302f
himanshugupta11002 <72141497+himanshugupta11002@users.noreply.github.com>
2024-03-31 10:20:45 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,8 +1,8 @@
-pip install langchain==0.0.154
-pip install PyPDF2==3.0.1
-pip install python-dotenv==1.0.0
-pip install streamlit==1.18.1
-pip install faiss-cpu==1.7.4
+pip install langchain
+pip install PyPDF2
+pip install python-dotenv
+pip install streamlit
+pip install faiss-cpu
 pip install streamlit-extras
 
 
